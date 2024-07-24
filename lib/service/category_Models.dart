bf08c77bc08e761ed80caa68dd2171b086c50545
Ahmed-Ewaida <168562147+Ahmed-Ewaida@users.noreply.github.com>
2024-07-25 02:50:05 +0300

    Add files via upload

---
+++ lib/service/category_Models.dart
@@ -0,0 +1,14 @@
+class CategoryModels{
+  final String name;
+  CategoryModels(this.name);
+}
+List<CategoryModels> CATEGORYS=[
+  CategoryModels("business"),
+  CategoryModels("health"),
+  CategoryModels("sports"),
+  CategoryModels("technology"),
+  CategoryModels("general"),
+  CategoryModels("entertainment"),
+  CategoryModels("science"),
+];
+
